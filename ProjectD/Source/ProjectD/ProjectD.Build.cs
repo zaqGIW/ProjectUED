8e599018bdd377513246d1cd885f34edb3fa90be
HAM <hamsh2003@naver.com>
2025-04-20 22:38:13 +0900

    Build.cs "AIModule" 추가

--- ProjectD/Source/ProjectD/ProjectD.Build.cs
+++ ProjectD/Source/ProjectD/ProjectD.Build.cs
@@ -8,7 +8,7 @@ public class ProjectD : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
